faf458f500e1c18bdd7c3cf217b929f9f04630e8
Cody Miller <cody.miller@learningfuze.com>
2020-07-26 17:16:24 -0700

    completed basic HTML & CSS layout, completed data model in user-data.js

---
+++ js/user-data.js
@@ -0,0 +1,42 @@
+var userData = {
+  profile: {
+    imageUrl: "images/headshot.jpg",
+    firstName: "Jeff",
+    lastName: "Jefferson",
+    age: 37,
+    gender: "Male",
+    location: "United States"
+  },
+  blogs: [
+    {
+      title: "Digging deeper into HTML!",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur commodi necessitatibus quisquam eius veniam a reiciendis rerum cumque optio aspernatur?"
+    },
+    {
+      title: "Digging deeper into CSS!",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur commodi necessitatibus quisquam eius veniam a reiciendis rerum cumque optio aspernatur?"
+    },
+    {
+      title: "Digging deeper into Javascript!",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur commodi necessitatibus quisquam eius veniam a reiciendis rerum cumque optio aspernatur?"
+    }
+  ],
+  images: [
+    {
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, esse. Item 1",
+      url: "images/headshot.jpg"
+    },
+    {
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, esse. Item 2",
+      url: "images/headshot.jpg"
+    },
+    {
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, esse. Item 3",
+      url: "images/headshot.jpg"
+    },
+    {
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, esse. Item 4",
+      url: "images/headshot.jpg"
+    }
+  ]
+}
